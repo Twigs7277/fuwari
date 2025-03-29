fa200b5db058551df6c861902436dae469d1b452
Twigs7277 <mastersonbutler60611@gmail.com>
2025-03-29 20:46:24 +0800

    cs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.jiwoo.us.kg/",
+  site: "https://blog.050413.xyz/",
   base: "/",
   trailingSlash: "always",
   integrations: [
