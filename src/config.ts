10bae41e272c87f8ac3208127dd6af8c719f4139
Twigs7277 <mastersonbutler60611@gmail.com>
2025-03-29 21:00:32 +0800

    qq

--- src/config.ts
+++ src/config.ts
@@ -43,8 +43,8 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: '超级小跟班',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      name: '进来看看嘛',
+      url: 'http://nezha.050413.xyz',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
