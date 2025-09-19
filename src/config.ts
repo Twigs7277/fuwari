4813d6f5ebd8a9a311729eb0c5a3c8c4588d2742
Twigs7277 <mastersonbutler60611@gmail.com>
2025-09-19 23:40:14 +0800

    qq

--- src/config.ts
+++ src/config.ts
@@ -43,7 +43,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: '进来看看嘛',
+      name: '进来',
       url: 'https://todo.050413.xyz',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
